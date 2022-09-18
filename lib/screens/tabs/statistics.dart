f1abfb6cb6e3b2e8059869ffc6766aed5021b88f
Trishakatam2 <85455705+Trishakatam2@users.noreply.github.com>
2022-09-18 21:44:21 +0530

    Add files via upload

---
+++ lib/screens/tabs/statistics.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Statistics extends StatefulWidget {
+  const Statistics({Key? key}) : super(key: key);
+
+  @override
+  _StatisticsState createState() => _StatisticsState();
+}
+
+class _StatisticsState extends State<Statistics> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [Container()],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
